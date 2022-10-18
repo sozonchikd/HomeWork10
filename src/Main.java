fcd64565bacb2538f3fdbc3811e0bebb619d7520
Sozonchik <den.sozon@mail.ru>
2022-10-18 20:21:49 +0500

    Сделал задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+//Задание 1.
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " +firstName + " " +middleName;
+        System.out.println(fullName);
+
+
+        System.out.println("Задание 2");
+
+
+
+
+
 
     }
 }
